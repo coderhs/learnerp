042367dacc2dc1b531fce46bf9bf9a1ac844843a
coderhs <mailme@hsps.in>
2012-11-12 16:27:46 +0530

    Program to import data from CSV

---
+++ populate.rb
@@ -0,0 +1,29 @@
+=begin
+
+population.rb is used to import a csv file to our DB, 
+which for development purpose in Sqlite
+
+To use the script u need to write
+
+$ ruby populate.rb data.csv
+
+The data should be in the form
+key,value
+key,value
+key,value
+	
+=end
+
+require 'sequel'
+DB = Sequel.connect("sqlite://data.db")
+items = DB[:items]
+if ARGV[0].nil? then
+	puts "NO FILE ENTERED \n\n "
+	puts "Usage eg:- ruby populate.rb data.csv\n\n"
+else
+	file = File.open ARGV[0]
+	file.each do |line|
+		data = line.split(',')
+		items.insert(:key => data[0].downcase,:value => data[1].downcase)
+	end
+end
