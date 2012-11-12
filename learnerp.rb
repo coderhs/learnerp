6e245ae2400644e0eba4274632303ca43d4b8ae9
coderhs <mailme@hsps.in>
2012-11-12 16:31:18 +0530

    enter the action /enter and /insert

--- learnerp.rb
+++ learnerp.rb
@@ -1,15 +1,61 @@
 require 'cuba'
 require 'cuba/render'
+require 'sequel'
+
 Cuba.plugin Cuba::Render
+
+Cuba.use Rack::Static,
+root: "./",
+urls: ["/assets"]
+
+DB = Sequel.connect("sqlite://data.db")
+items = DB[:items]
 
 Cuba.define do
 	on root do
 		on param('txtweb-message') do |msg|
-			res.write render('response.html.erb',data:msg)
+			dataset = items.where(:key => msg.downcase)
+			data = "Not present in DB"
+			dataset.each do |item|
+				data = item[:value]
+			end
+			res.write render('response.html.erb',data:data)
 		end
 
 		on default do
 			res.write render('response.html.erb',data:"data")
+		end
+	end
+
+	on "enter" do
+		on post do
+			on param('data') do |data|
+				res.write data
+				p data.scan(/<p>(.*?)<\/p>/imu).flatten.flatten.to_s.split("<br />")[0]
+				p "here"
+			end
+		end
+		on default do
+			res.write render('submit_form.html.erb')
+		end
+	end
+
+	on "insert" do
+		on get do
+			res.write render('insert.html.erb')
+		end
+		on post do
+			on param('key'),param('value') do |key,value|
+				items.insert(:key => key, :value => value)
+				res.write "key: #{key} with value: #{value} has been inserted"
+			end
+		end
+	end
+
+	on "view" do
+		on get do
+			items = DB[:items]
+			res.write render('view.html.erb',data:items)
 		end
 	end
 end
